36e02019743cdde1f14e1aded68eec025da5fab4
anhkhoi <109712876+khoidesu@users.noreply.github.com>
2024-03-10 16:13:29 +0700

    uplooad

---
+++ copy.py
@@ -0,0 +1,25 @@
+import shutil
+
+file_to_copy = 'D:/CPP/.MAIN/main.cpp'
+
+destination_directory = 'D:/chambai/bailam/khoidesu/TASK.cpp'
+
+shutil.copy(file_to_copy, destination_directory)
+
+#===========================================================#
+
+file_to_copy = 'D:/CPP/.MAIN/TASK.inp'
+
+destination_directory = 'D:/chambai/botest/TASK/TEST1/TASK.inp'
+
+shutil.copy(file_to_copy, destination_directory)
+
+#===========================================================#
+
+file_to_copy = 'D:/CPP/.MAIN/TASK.out'
+
+destination_directory = 'D:/chambai/botest/TASK/TEST1/TASK.out'
+
+shutil.copy(file_to_copy, destination_directory)
+
+#===========================================================#
